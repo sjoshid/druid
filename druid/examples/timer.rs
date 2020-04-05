8d46f6ab64e57f499d08e863592ef606b0064373
sjoshid <ILGitHub2>
2020-04-04 21:31:14 -0400

    1) Changing timers.rs to its original form. 2) Adding my own timers version to play around with.

--- druid/examples/timer.rs
+++ druid/examples/timer.rs
@@ -17,24 +17,12 @@
 use std::time::{Duration, Instant};
 
 use druid::kurbo::Line;
-use druid::{
-    AppLauncher, BoxConstraints, Color, Env, Event, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx,
-    LocalizedString, PaintCtx, RenderContext, Size, TimerToken, UpdateCtx, Widget, WindowDesc,
-};
-use druid::widget::{WidgetExt, Flex};
+use druid::widget::prelude::*;
+use druid::{AppLauncher, Color, LocalizedString, TimerToken, WindowDesc};
 
 struct TimerWidget {
     timer_id: TimerToken,
     on: bool,
-}
-
-impl TimerWidget {
-    fn new() -> TimerWidget {
-        TimerWidget {
-            timer_id: TimerToken::INVALID,
-            on: false,
-        }
-    }
 }
 
 impl Widget<u32> for TimerWidget {
@@ -50,11 +38,8 @@ impl Widget<u32> for TimerWidget {
                 if *id == self.timer_id {
                     self.on = !self.on;
                     ctx.request_paint();
-                    println!("Received/triggering by id: {:?}", ctx.widget_id());
-                    let deadline = Instant::now() + Duration::from_millis(2000);
+                    let deadline = Instant::now() + Duration::from_millis(500);
                     self.timer_id = ctx.request_timer(deadline);
-                } else {
-                    println!("Ignored by id: {:?}", ctx.widget_id());
                 }
             }
             _ => (),
@@ -75,58 +60,18 @@ impl Widget<u32> for TimerWidget {
         bc.constrain((100.0, 100.0))
     }
 
-    fn paint(&mut self, paint_ctx: &mut PaintCtx, _data: &u32, _env: &Env) {
+    fn paint(&mut self, ctx: &mut PaintCtx, _data: &u32, _env: &Env) {
         if self.on {
-            paint_ctx.stroke(Line::new((10.0, 10.0), (10.0, 50.0)), &Color::WHITE, 1.0);
+            ctx.stroke(Line::new((10.0, 10.0), (10.0, 50.0)), &Color::WHITE, 1.0);
         }
     }
 }
 
-fn build_widget() -> impl Widget<u32> {
-    let t1 = TimerWidget::new().debug_widget();
-    let t2 = TimerWidget::new().debug_widget();
-    let t3 = TimerWidget::new().debug_widget();
-    let t4 = TimerWidget::new().debug_widget();
-    //let label = Label::new(|data: &String, _env: &_| format!("value: {}", data));
-
-    let column1 = Flex::column()
-        .with_child(t1, 1.0)
-        .with_child(t2, 1.0)
-        .with_child(t3, 1.0)
-        .with_child(t4, 1.0);
-
-    let t5 = TimerWidget::new().debug_widget();
-    let t6 = TimerWidget::new().debug_widget();
-    let t7 = TimerWidget::new().debug_widget();
-    let t8 = TimerWidget::new().debug_widget();
-    //let label = Label::new(|data: &String, _env: &_| format!("value: {}", data));
-
-    let column2 = Flex::column()
-        .with_child(t5, 1.0)
-        .with_child(t6, 1.0)
-        .with_child(t7, 1.0)
-        .with_child(t8, 1.0);
-
-    let t9 = TimerWidget::new().debug_widget();
-    let t10 = TimerWidget::new().debug_widget();
-    let t11 = TimerWidget::new().debug_widget();
-    let t12 = TimerWidget::new().debug_widget();
-    //let label = Label::new(|data: &String, _env: &_| format!("value: {}", data));
-
-    let column3 = Flex::column()
-        .with_child(t9, 1.0)
-        .with_child(t10, 1.0)
-        .with_child(t11, 1.0)
-        .with_child(t12, 1.0);
-
-    let root = Flex::row().with_child(column1, 1.0).with_child(column2, 1.0)
-        .with_child(column3, 1.0);
-
-    root
-}
-
 fn main() {
-    let window = WindowDesc::new(build_widget)
+    let window = WindowDesc::new(|| TimerWidget {
+        timer_id: TimerToken::INVALID,
+        on: false,
+    })
     .title(LocalizedString::new("timer-demo-window-title").with_placeholder("Tick Tock"));
 
     AppLauncher::with_window(window)
