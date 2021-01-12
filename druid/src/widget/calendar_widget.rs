5d86b1d4df45d0a130678eb94e1952cfd3b454b8
Sujit <joshi.sujit020386@gmail.com>
2021-01-12 18:03:25 -0500

    Padding, borders, etc. works fine.

--- druid/src/widget/calendar_widget.rs
+++ druid/src/widget/calendar_widget.rs
@@ -9,7 +9,7 @@ pub struct CalendarDateWidget {
 	days_widget: Vec<WidgetPod<String, Container<String>>>,
 	//su, mo, tu, etc.
 	// date of month cannot be a const. it changes per month
-	dates_of_month_widget: Vec<WidgetPod<DateDetails, CustomDateWrapper>>, // this will be used to highlight.
+	dates_of_month_widget: Vec<WidgetPod<DateDetails, DateWrapper>>, // this will be used to highlight.
 }
 
 impl CalendarDateWidget {
@@ -32,33 +32,25 @@ impl CalendarDateWidget {
 	}
 }
 
-/// Wraps a Label in a Container.
-/// I chose Container because it takes a &mut that adds a border. Not sure if this is the right choice.
-struct CustomDateWrapper {
-	label_in_container: Container<DateDetails>,
-	draw_border: bool,
-	grey_date: bool,
+struct InnerDate {
+	dynamic_label: Container<DateDetails>,
 }
 
-impl CustomDateWrapper {
-	pub fn new(date_details: DateDetails) -> Self {
-		let dynamic_label = Label::dynamic(|data: &u32, _| data.to_string()).center().lens(DateDetails::date);
-
-		let label_in_container = Container::new(dynamic_label.padding(3.));
-
-		CustomDateWrapper {
-			label_in_container,
-			draw_border: date_details.draw_border,
-			grey_date: date_details.grey_date,
+impl InnerDate {
+	pub fn new() -> InnerDate {
+		InnerDate {
+			dynamic_label: Container::new(Label::dynamic(|data: &DateDetails, _| data.date.to_string()).center())
 		}
 	}
 }
 
-impl Widget<DateDetails> for CustomDateWrapper {
+impl Widget<DateDetails> for InnerDate {
 	fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut DateDetails, env: &Env) {
+		println!("cklicked inner date");
 		match event {
 			Event::MouseDown(mouse_event) => {
 				if mouse_event.button == MouseButton::Left {
+					println!("clicked date");
 					ctx.set_active(true);
 				}
 			}
@@ -74,29 +66,84 @@ impl Widget<DateDetails> for CustomDateWrapper {
 	fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &DateDetails, env: &Env) {
 		match event {
 			LifeCycle::WidgetAdded => {
-				self.label_in_container.lifecycle(ctx, event, data, env);
+				self.dynamic_label.lifecycle(ctx, event, data, env);
 			}
 			_ => {}
 		}
 	}
 
 	fn update(&mut self, ctx: &mut UpdateCtx, old_data: &DateDetails, data: &DateDetails, env: &Env) {
-		self.label_in_container.update(ctx, old_data, data, env);
+		self.dynamic_label.update(ctx, old_data, data, env);
 	}
 
 	fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, data: &DateDetails, env: &Env) -> Size {
-		self.label_in_container.layout(ctx, bc, data, env)
+		self.dynamic_label.layout(ctx, bc, data, env)
+	}
+
+	fn paint(&mut self, ctx: &mut PaintCtx, data: &DateDetails, env: &Env) {
+		let grey_date = data.grey_date;
+		if grey_date {
+			println!("grey date");
+		} else {
+			println!("normal date color");
+		}
+
+		if data.date_is_todays {
+			println!("date is todays. background is green");
+			self.dynamic_label.set_background(Color::rgb(0., 1., 0.));
+		} else {
+			self.dynamic_label.remove_background();
+			println!("date is NOT todays. background is normal");
+		}
+		self.dynamic_label.paint(ctx, data, env);
+	}
+}
+
+/// Wraps a Label in a Container.
+/// I chose Container because it takes a &mut that adds a border. Not sure if this is the right choice.
+struct DateWrapper {
+	date_wrapper: Container<DateDetails>,
+	draw_border: bool,
+}
+
+impl DateWrapper {
+	pub fn new(date_details: DateDetails) -> Self {
+		let inner_date_widget = InnerDate::new();
+
+		let date_wrapper = Container::new(inner_date_widget.padding(3.));
+
+		DateWrapper {
+			date_wrapper,
+			draw_border: date_details.draw_border,
+		}
+	}
+}
+
+impl Widget<DateDetails> for DateWrapper {
+	fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut DateDetails, env: &Env) {
+		self.date_wrapper.event(ctx, event, data, env);
+	}
+
+	fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &DateDetails, env: &Env) {
+		self.date_wrapper.lifecycle(ctx, event, data, env);
+	}
+
+	fn update(&mut self, ctx: &mut UpdateCtx, old_data: &DateDetails, data: &DateDetails, env: &Env) {
+		self.date_wrapper.update(ctx, old_data, data, env);
+	}
+
+	fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, data: &DateDetails, env: &Env) -> Size {
+		self.date_wrapper.layout(ctx, bc, data, env)
 	}
 
 	fn paint(&mut self, ctx: &mut PaintCtx, data: &DateDetails, env: &Env) {
 		let border = data.draw_border;
 		if border {
-			self.label_in_container.set_border(Color::rgb(0., 1., 0.), 2.);
+			self.date_wrapper.set_border(Color::rgb(0., 1., 0.), 2.);
 		} else {
-			//sj_todo there is no way to remove a border. But we can set its color to same as widget bg color.
-			self.label_in_container.set_border(Color::rgb(1., 0., 0.), 2.);
+			self.date_wrapper.remove_border();
 		}
-		self.label_in_container.paint(ctx, data, env);
+		self.date_wrapper.paint(ctx, data, env);
 	}
 }
 
@@ -107,6 +154,7 @@ impl Widget<CalendarData> for CalendarDateWidget {
 				for (i, dynamic_date) in self.dates_of_month_widget.iter_mut().enumerate() {
 					dynamic_date.event(ctx, event, &mut data.all_dates[i], env);
 					if dynamic_date.has_active() { // we cant have date_widget.is_active()
+						println!("has active");
 						if data.active_date_details_index.is_some() {
 							data.inactive_date_details_index = data.active_date_details_index;
 						}
@@ -158,7 +206,7 @@ impl Widget<CalendarData> for CalendarDateWidget {
 						is_current_month = true;
 					}
 
-					let mut date_widget = WidgetPod::new(CustomDateWrapper::new(d.clone()));
+					let mut date_widget = WidgetPod::new(DateWrapper::new(d.clone()));
 					self.dates_of_month_widget.push(date_widget);
 				}
 
